eaed947ee24d9538f70200aa69c3da93a1dc0d56
ueneid <eidhie@gmail.com>
2023-12-04 11:42:31 +0900

    Refactored Day1 part1

--- src/main/java/com/ueneid/Day1.java
+++ src/main/java/com/ueneid/Day1.java
@@ -1,12 +1,8 @@
 package com.ueneid;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Day1 {
     private final List<String> input;
 
@@ -15,21 +11,24 @@ public class Day1 {
     }
 
     public int part1() {
-        ArrayList<Integer> values = new ArrayList<>();
-        for (int i = 0; i < input.size(); i++) {
-            ArrayList<Character> tmp = new ArrayList<>();
-            for (var code : input.get(i).toCharArray()) {
-                if (Character.isDigit(code)) {
-                    tmp.add(code);
-                }
-            }
-            values.add(Integer.parseInt(tmp.getFirst().toString() + tmp.getLast().toString()));
-        }
-        return values.stream().mapToInt(Integer::intValue).sum();
+        return input.stream()
+                    .map(this::extractFirstAndLastDigit)
+                    .mapToInt(Integer::valueOf)
+                    .sum();
+    }
+
+    private int extractFirstAndLastDigit(String str) {
+        String numbers = str.chars()
+                            .filter(Character::isDigit)
+                            .mapToObj(c -> String.valueOf((char) c))
+                            .collect(Collectors.joining());
+
+        return Integer.parseInt("" + numbers.charAt(0) + numbers.charAt(numbers.length() - 1));
     }
 
     public static void main(String[] args) {
-        var input = Resource.resourceAsListOfString("input/day1/sample.txt");
+        var input = Resource.resourceAsListOfString("input/day1/input.txt");
         var answer1 = new Day1(input).part1();
+        System.out.println(answer1);
     }
 }
